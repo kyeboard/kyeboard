f8bfe745d2e6896981aa8b47117d4d5241787269
tokayakii <tokayaki01@gmail.com>
2022-10-16 19:06:37 +0530

    Updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,12 @@ import SideBar from "../components/SideBar.vue"
 <template>
 <SideBar />
 <div class="w-screen h-screen bg-base flex items-center justify-center text-text">
-
+    <div class="right w-1/3 relative">
+        <div class="bg-gradient-to-r from-[#eecda3] to-[#ef629f] w-fit text-transparent bg-clip-text font-bold">Watashi no saito e yōkoso</div>
+        <div class="text-6xl font-bold mt-4 leading-[70px]">
+            A fullstack and Android developer
+            <span class="bg-gradient-to-r from-[#eecda3] to-[#ef629f] w-fit text-transparent bg-clip-text font-bold">.</span>
+        </div>
+    </div>
 </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                floating: "floating 1s ease-in-out infinite"
+            },
+            keyframes: {
+                floating: {
+                    "0%, 100%": {
+                        transform: "translateY(0px)"
+                    },
+                    "50%": {
+                        transform: "translateY(10px)"
+                    }
+                }   
+            }
+        },
     },
     plugins: [
         require("@catppuccin/tailwindcss")({
