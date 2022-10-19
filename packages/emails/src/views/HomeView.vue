9fec25e6b88d91d007aaf698a3c35e8b6e23a1d0
tokayakii <tokayaki01@gmail.com>
2022-10-19 07:11:32 +0530

    feat(admin): Add beautiful login page

--- packages/emails/src/views/HomeView.vue
+++ packages/emails/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
+<div class="w-screen h-screen bg-base flex items-center justify-center flex-col text-text">
+    <div class="text-4xl font-extrabold">Welcome to Admin Panel!</div>
+    <span class="font-bold text-left w-1/2 mt-6">Passphrase</span>
+    <input class="bg-surface0/40 p-4 w-1/2 mt-2 rounded-xl px-6 placeholder-overlay1 font-semibold" placeholder="Enter the passphrase..." />
+    <button class="bg-gradient-to-r from-[#eecda3] to-[#ef629f] w-1/2 rounded-xl mt-4 text-base font-bold p-4">Authenticate</button>
+</div>
 </template>
--- packages/emails/tailwind.config.js
+++ packages/emails/tailwind.config.js
@@ -2,7 +2,53 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                floating: "floating 1s ease-in-out infinite",
+                grow: "grow 0.6s ease-in-out forwards",
+                fade_in: "fade_in 0.6s ease-in-out forwards"
+            },
+            keyframes: {
+                floating: {
+                    "0%, 100%": {
+                        transform: "translateY(0px)",
+                    },
+                    "50%": {
+                        transform: "translateY(10px)",
+                    },
+                },
+                grow: {
+                    "0%": {
+                        height: "0px",
+                        opacity: 0.9
+                    },
+                    "100%": {
+                        height: "100vh",
+                        opacity: 1
+                    },
+                },
+                fade_in: {
+                    "0%": {
+                        opacity: 0,
+                        transform: "translateY(-10px)"
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translateY(0px)"
+                    },
+                },
+            },
+            screens: {
+                sm: "730px",
+                sd: "1050px",
+                md: "1350px"
+            }
+        },
     },
-    plugins: [],
+    plugins: [
+        require("@catppuccin/tailwindcss")({
+            prefix: "",
+            defaultFlavour: "mocha",
+        }),
+    ],
 };
