f48e27bead35fca791758deb663e6001ecc6f3ac
tokayakii <tokayaki01@gmail.com>
2022-10-20 06:31:21 +0530

    feat: Add super beautiful home page for the admin console

--- packages/emails/tailwind.config.js
+++ packages/emails/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
             animation: {
                 floating: "floating 1s ease-in-out infinite",
                 grow: "grow 0.6s ease-in-out forwards",
-                fade_in: "fade_in 0.6s ease-in-out forwards"
+                fade_in: "fade_in 0.6s ease-in-out forwards",
             },
             keyframes: {
                 floating: {
@@ -20,29 +20,29 @@ module.exports = {
                 grow: {
                     "0%": {
                         height: "0px",
-                        opacity: 0.9
+                        opacity: 0.9,
                     },
                     "100%": {
                         height: "100vh",
-                        opacity: 1
+                        opacity: 1,
                     },
                 },
                 fade_in: {
                     "0%": {
                         opacity: 0,
-                        transform: "translateY(-10px)"
+                        transform: "translateY(-10px)",
                     },
                     "100%": {
                         opacity: 1,
-                        transform: "translateY(0px)"
+                        transform: "translateY(0px)",
                     },
                 },
             },
             screens: {
                 sm: "730px",
                 sd: "1050px",
-                md: "1350px"
-            }
+                md: "1350px",
+            },
         },
     },
     plugins: [
